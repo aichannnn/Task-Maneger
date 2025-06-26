63a78b99b68b761d21f190070bb851c463fa978c
Priyanka Singh <92680404+aichannnn@users.noreply.github.com>
2025-06-27 03:07:11 +0530

    Update TaskManager.jsx

--- client/src/components/TaskManager.jsx
+++ client/src/components/TaskManager.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { jwtDecode } from "jwt-decode";
 import axios from 'axios';
 
-const API = "https://task-backend-vviq.onrender.com";
+const API = "https://backend-task-x8ax.onrender.com";
 
 function TaskManager() {
   const [tasks, setTasks] = useState([]);
